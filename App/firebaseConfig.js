472b499b8d42e516df08612950c26cb3d8434cb2
Priyansh Singh Tanwar <154755072+Priyansh6747@users.noreply.github.com>
2025-09-20 00:58:45 +0530

    Setup TS

---
+++ App/firebaseConfig.js
@@ -0,0 +1,23 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { initializeAuth, getReactNativePersistence } from 'firebase/auth';
+import ReactNativeAsyncStorage from '@react-native-async-storage/async-storage';
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCeokxYRTUlWCnJI8ZRtg-w-KzKn8IB1W4",
+    authDomain: "nutrilio-ea247.firebaseapp.com",
+    projectId: "nutrilio-ea247",
+    storageBucket: "nutrilio-ea247.firebasestorage.app",
+    messagingSenderId: "470528429648",
+    appId: "1:470528429648:web:8e9c3b58253f2345950b55",
+    measurementId: "G-XZP9DYPJFY"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+export const analytics = getAnalytics(app);
+export const auth = initializeAuth(app, {
+    persistence: getReactNativePersistence(ReactNativeAsyncStorage)
+});
